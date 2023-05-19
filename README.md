031864beab2e3bcd0e025e83d2f779a73c45826e
Kings-Israel <47811074+Kings-Israel@users.noreply.github.com>
2023-05-19 06:02:18 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Change vue version to at least 2.6.x in package.json
+
+"vue": "2.6.x"
+
+# Change vue-template version to at least 2.6.x in package.json
+
+"vue-template-compiler": "2.6.x"
+
+# Run npm install in terminal
+
+npm install
+
+# Add dev server to vue.config.js for custom dev server
+
+devServer: {
+  host: "admin.services.test"
+}
+
+# Run npm run server to serve the app
+
+npm run server
---
+++ src/store/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import createPersistedState from 'vuex-persistedstate'
+
+// Modules
+import ecommerceStoreModule from '@/views/apps/e-commerce/eCommerceStoreModule'
+import app from './app'
+import appConfig from './app-config'
+import verticalMenu from './vertical-menu'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  modules: {
+    app,
+    appConfig,
+    verticalMenu,
+    'app-ecommerce': ecommerceStoreModule,
+  },
+  // strict: process.env.DEV,
+  plugins: [createPersistedState()],
+})
