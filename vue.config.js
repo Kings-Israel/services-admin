c9c9761d47916d3d5fe7f96a36b720a98f683d07
Kings-Israel <47811074+Kings-Israel@users.noreply.github.com>
2023-05-19 07:36:00 +0300

    Initial setup

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === 'development' ? '/' : '',
+    process.env.NODE_ENV === 'development' ? '/' : '/services-admin',
   lintOnSave: false,
   devServer: {
     host: 'admin.services.test',
